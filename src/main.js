ce80a0d6300745102f26c026fc863bd57d4bca78
leear <pooa8@naver.com>
2021-02-13 02:20:02 +0900

    Feat : Create a page router manually - login template, 404 template 생성 및 라우터 직접 구현

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+const Login = { template: '<div>Login Page</div>' }
+
+const routes = {
+  '/': App,
+  '/login': Login  // login component
+}
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  computed: {
+    VueComponent() {
+      // path 경로를 route map 에서 찾고, 없으면 404 template return
+      return routes[window.location.pathname] ||
+        { template: '<div>Page not found</div>' }
+    }
+  },
+  render(h) {
+    return h(this.VueComponent)
+  }
 })
