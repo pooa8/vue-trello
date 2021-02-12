134780642e97817a184a2363d5059e9991bfd1d0
leear <pooa8@naver.com>
2021-02-13 03:07:27 +0900

    Feat : Use router-view and router-link component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
-    여기서부터 코드를 시작합니다!
-    
+    <Navbar />
     <!-- 라우팅에 따라 컴포넌트가 대체되는 부분 -->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
+
 export default {
   name: 'app',
+  components: { Navbar },
   data () {
     return {
     }
