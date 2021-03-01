02a24574b9b03ebe240b6674f9670a47d0b9f13a
leear <pooa8@naver.com>
2021-03-01 23:06:26 +0900

    Feat : Update Card title/description - Modal 팝업으로 뜨는 카드 조회화면에서 title 과 description 수정 기능   추가

--- src/api/index.js
+++ src/api/index.js
@@ -47,5 +47,8 @@ export const card = {
     },
     create(title, listId, pos) {
         return request('post', '/cards', {title, listId, pos}) // 객체로 전달
+    },
+    update(id, payload) {
+        return request('put', `/cards/${id}`, payload)
     }
 }
