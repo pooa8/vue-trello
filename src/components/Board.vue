2c2320a92f67a458580d1f5fb83506f93a7443d0
leear <pooa8@naver.com>
2021-03-04 23:57:04 +0900

    Refactor : Refactoring Board.vue and Add dragger.js - /utils/dragger.js 파일 신규 생성 - Board.vue 의 카드 이동 부분 리팩토링 및 dragula 관련 부분 dragger.js   파일에 추가

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -21,8 +21,7 @@
 <script>
 import {mapState, mapActions} from 'vuex'
 import List from './List.vue'
-import dragula from 'dragula'
-import 'dragula/dist/dragula.css'
+import dragger from '../utils/dragger'
 
 export default {
     components: {
@@ -32,7 +31,7 @@ export default {
         return {
             bid: 0,
             loading: false,
-            dragulaCards: null
+            cDragger: null
         }
     },
     computed: {
@@ -44,48 +43,41 @@ export default {
         this.fetchData()
     },
     updated() {
-      if (this.dragulaCards) this.dragulaCards.destroy()
-      
-      this.dragulaCards = dragula([
-        ...Array.from(this.$el.querySelectorAll('.card-list'))
-      ]).on('drop', (el, wrapper, target, silblings) => {
-        const targetCard = {
-          id: el.dataset.cardId * 1, 
-          pos: 65535,
-        }
-        let prevCard = null
-        let nextCard = null
-        Array.from(wrapper.querySelectorAll('.card-item'))
-          .forEach((el, idx, arr) => {
-            const cardId = el.dataset.cardId * 1
-            if (targetCard.id === cardId) {
-              prevCard = idx > 0 ? {
-                id: arr[idx - 1].dataset.cardId * 1,
-                pos: arr[idx - 1].dataset.cardPos * 1,
-              } : null
-              nextCard = idx < arr.length - 1 ? {
-                id: arr[idx + 1].dataset.cardId * 1,
-                pos: arr[idx + 1].dataset.cardPos * 1,
-              } : null
-            }
-          })
-        if (!prevCard && nextCard) targetCard.pos = nextCard.pos / 2
-        else if (!nextCard && prevCard) targetCard.pos = prevCard.pos * 2
-        else if (nextCard && prevCard) targetCard.pos = (prevCard.pos + nextCard.pos) / 2
-        this.UPDATE_CARD(targetCard)
-      })
+      this.setCardDraggable() // Board 의 자식 컴포넌트가 모두 렌더링 된 후 호출
     },
     methods: {
-        ...mapActions([
-            'FETCH_BOARD',
-            'UPDATE_CARD'
-        ]),
-        fetchData() { // 백엔드 API를 호출하고, 데이터를 요청하는 함수
-            this.loading = true
-            this.FETCH_BOARD({id: this.$route.params.bid})
-              .then(() => this.loading = false)
-        }
+      ...mapActions([
+          'FETCH_BOARD',
+          'UPDATE_CARD'
+      ]),
+      fetchData() { // 백엔드 API를 호출하고, 데이터를 요청하는 함수
+          this.loading = true
+          this.FETCH_BOARD({id: this.$route.params.bid})
+            .then(() => this.loading = false)
+      },
+      setCardDraggable() {
+        if(this.cDragger) this.cDragger.destroy()
+        this.cDragger = dragger.init(Array.from(this.$el.querySelectorAll('.card-list')))
+        this.cDragger.on('drop', (el, wrapper, target, silblings) => {
+          const targetCard = {
+            id: el.dataset.cardId * 1, 
+            pos: 65535,
+          }
+          const {prev, next} = dragger.silblings({
+            el,
+            wrapper,
+            candidate: Array.from(wrapper.querySelectorAll('.card-item')),
+            type: 'card'
+          })
+
+          if (!prev && next) targetCard.pos = next.pos / 2
+          else if (!next && prev) targetCard.pos = prev.pos * 2
+          else if (next && prev) targetCard.pos = (prev.pos + next.pos) / 2
+  
+          this.UPDATE_CARD(targetCard)
+      })
     }
+  }
 }
 </script>
 
